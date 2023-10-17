eec59fbf0e4f21cf08bc76b41e3002ec52e9f4dd
frans.mazhar <frans.mazhar@gmail.com>
2023-10-17 08:32:21 +0700

    update

--- src/main/java/fdm/registry/ServiceRegistryApplication.java
+++ src/main/java/fdm/registry/ServiceRegistryApplication.java
@@ -2,12 +2,10 @@ package fdm.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+//import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-@EnableAdminServer
-@EnableEurekaServer
+//@EnableAdminServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
